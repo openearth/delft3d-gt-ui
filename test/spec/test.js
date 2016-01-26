2be2bc0920624d0730fdc49ed6e776d6e194b86a
Fedor Baart <fedor.baart@deltares.nl>
2016-01-26 23:28:18 +0100

    tests for current issues (both in node and in browser)

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,7 +1,21 @@
+/* global chai */
 (function () {
   'use strict';
-  var assert = require('chai').assert
+  if (typeof require !== 'undefined') {
+    // stuff to test without a browser
+    var assert = require('chai').assert;
+    var nock = require('nock');
+    var fetch = require('node-fetch');
+    nock('https://delft3d-gt')
+          .post('/runs')
+          .reply(200, {
+            uuid: 'XXXX-XXXX'
+          });
 
+  } else {
+    assert = chai.assert;
+    fetch = window.fetch;
+  }
   describe('If we are on the create run page', function () {
     describe('And we press the create button ', function () {
       it('should send a json message to the server ', function () {
@@ -12,6 +26,29 @@
       });
       it('should have a timestep paramater (dt) with a numeric value', function () {
         assert(false, 'implement and test dt in format');
+      });
+      it('should be posted to the server, resulting in a  uuid', function(done){
+        fetch('https://delft3d-gt/runs', {
+          method: 'POST',
+          body: JSON.stringify({
+            'name': 'f34'
+          })
+        })
+          .then(function(response){
+            console.log(response);
+            return response.json();
+          })
+          .catch(function(error){
+            done(error);
+          })
+          .then(function(data) {
+            assert.ok(data.uuid, 'got some data with a uuid');
+            done();
+          })
+          .catch(function(error){
+            done(error);
+          });
+
       });
     });
   });
