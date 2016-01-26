91396c134da94b9331aadf1eb0737b1d6d68321c
Fedor Baart <fedor.baart@deltares.nl>
2016-01-26 21:48:14 +0100

    tests for current issues

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,17 @@
 (function () {
   'use strict';
+  var assert = require('chai').assert
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-
+  describe('If we are on the create run page', function () {
+    describe('And we press the create button ', function () {
+      it('should send a json message to the server ', function () {
+        assert(false, 'implement json message format');
+      });
+      it('should contain a name in the message', function () {
+        assert(false, 'implement and test name in format');
+      });
+      it('should have a timestep paramater (dt) with a numeric value', function () {
+        assert(false, 'implement and test dt in format');
       });
     });
   });
