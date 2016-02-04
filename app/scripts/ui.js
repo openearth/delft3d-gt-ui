d9d11f3d002ca4e36ebcd69c2a7c8f63e732b2dc
almar <jln@deltares.nl>
2016-02-04 09:48:18 +0100

    Fixed small issue that GUI did not load at startup properly (missing ref)
    Fixed small issue that GUI did not load at startup properly (missing
    ref)

--- app/scripts/ui.js
+++ app/scripts/ui.js
@@ -54,7 +54,7 @@
       str += "<tr id='model-" + model.uuid + "' class='" + model.status + "'>";
       str += "<td>" + model.name + "</td>";
       str += "<td>" + model.status + " " + model.progress + "%</td>";
-      str += "<td>" + model.timeleft + "</td>";
+    // Temporary hidden.  str += "<td>" + model.timeleft + "</td>";
       str += "<td class='column-actions'><button class='btn btn-border btn-small btn-model-delete' data-uuid='" + model.uuid + "'><span class='glyphicon glyphicon-remove' ></span></button></td>";
 
       str += "</tr>";
