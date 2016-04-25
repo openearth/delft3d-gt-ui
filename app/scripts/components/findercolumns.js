d9287bbf8c248483cebe7807890f496fb9001169
Fedor Baart <fedor.baart@deltares.nl>
2016-04-25 14:38:27 +0200

    implement three column component

--- app/scripts/components/findercolumns.js
+++ app/scripts/components/findercolumns.js
@@ -5,7 +5,29 @@ var exports = (function () {
   // Our homepage component
   var FinderColumns = Vue.component("finder-columns", {
     // not much in here.
-    template: "#template-finder-columns"
+    template: "#template-finder-columns",
+    data: function() {
+      return {
+        modelid: -1
+      };
+    },
+    components: {
+      "scenario-list": ScenarioList,
+      "model-list": ModelList,
+      "model-details": ModelDetails
+    },
+    route: {
+      data: function(transition) {
+        console.log("new finder column data", this.$children);
+        var modelDetails = this.$children[2];
+        var newData = {
+          modelid: parseInt(transition.to.params.modelid)
+        };
+
+        modelDetails.id = newData.modelid;
+        transition.next(newData);
+      }
+    }
   });
 
   return {
