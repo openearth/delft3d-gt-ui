99882ecc976ba5615f924bc440a79c53d9f47d70
almar <jln@deltares.nl>
2016-04-29 11:14:32 +0200

    Delete scenario now gives a confirm + scrollbar for scenario list
    Delete scenario now gives a confirm + scrollbar for scenario list

--- app/scripts/components/findercolumns.js
+++ app/scripts/components/findercolumns.js
@@ -57,7 +57,51 @@ var exports = (function () {
         }
 
         return null;
-      }
+      },
+
+      // Remove item, based on incoming modelinfo.
+      removeScenario: function() {
+
+        // keep track of the scenario before deletion
+        var scenarioId = this.scenario;
+        var that = this;
+
+        $("#dialog-remove-scenario-name").html("");
+        // Do we also remove all the additional files? This is based on the checkmark.
+        // if deletefiles is true, we will tell the server that we want to remove these files.
+        var deletefiles = true; // We do not provide this option anymore. LEaving it here shortly if someone changes his or her mind: $("#simulation-control-check-delete-files").is(":checked");
+
+          // User accepts deletion:
+        $("#dialog-remove-scenario-response-accept").on("click", () => {
+          var deletedId = this.selectedScenarioId;
+
+          var options = {};
+
+          deleteScenario(deletedId, options)
+            .then(function(data) {
+              console.log("scenario deleted from server", deletedId, "data:", data);
+
+              that.$parent.$broadcast("show-alert", { message: "Deleting scenario... It might take a moment before the view is updated.", showTime: 5000, type: "success"});
+
+            })
+            .catch(e => {
+              console.log("scenario deletion failed", e);
+            });
+
+          // Hide dialog when user presses this accept.:
+          $("#dialog-confirm-delete-scenario").modal("hide");
+
+          // key values correspond to url parameters which are lowercase
+          var params = {
+            scenarioid: scenarioId
+          };
+
+        });
+
+        // Show the dialog:
+        $("#dialog-confirm-delete-scenario").modal({});
+      },
+
     }
 
   });
