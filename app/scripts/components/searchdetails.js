649bb8e534fbd41d862acac1f562be59b98b48cf
Fedor Baart <fedor.baart@deltares.nl>
2016-06-06 21:31:42 +0200

    get form content from templates

--- app/scripts/components/searchdetails.js
+++ app/scripts/components/searchdetails.js
@@ -8,47 +8,19 @@ var exports = (function () {
     data: function() {
       return {
         selectedTemplates: [],
-        selectedModelEngines: [],
+        templates: [],
         parameters: {
           "river-width": null,
-          "river-discharge": null
+          "river-discharge": null,
+          "engines": []
         },
-        audience: {
-          "private": true
-        },
+        shared: [],
         startDate: null,
         endDate: null,
         text: ""
 
       };
     },
-    props: {
-      templates: {
-        default: function () {
-          return [
-            {
-              name: "Template A"
-            },
-            {
-              name: "Template B"
-            }
-          ];
-        }
-      },
-      modelEngines: {
-        default: function () {
-          return [
-            {
-              name: "Delft3D Curvilinear"
-            },
-            {
-              name: "Delft3D Flexible Mesh"
-            }
-          ];
-        }
-      }
-    },
-
     ready: function() {
       $(".ion-range").ionRangeSlider({
         onFinish: () => {
@@ -56,10 +28,39 @@ var exports = (function () {
           this.search();
         }
       });
+      fetchTemplates()
+        .then((templates) => {
+          console.log("loaded templates", templates);
+          this.templates = templates;
+          this.$nextTick(
+            function() {
+              // once the dom is updated, update the select pickers by hand
+              // template data is computed into modelEngine
+              $("#template").selectpicker("refresh");
+              $("#template").selectpicker("selectAll");
+              $("#model-engine").selectpicker("refresh");
+              $("#model-engine").selectpicker("selectAll");
 
+            }
+          );
+
+        });
       $(".select-picker").selectpicker();
+
     },
     computed: {
+      modelEngines: {
+        get: function () {
+          // flatten variables
+          var variables = _.flatMap(_.flatMap(this.templates, "sections"), "variables");
+          // lookup all variables with id engine (convention)
+          var engines = _.filter(variables, ["id", "engine"]);
+          // lookup default values (filter on scenes/scenarios later)
+          var defaultEngines = _.uniq(_.map(engines, "default"));
+          return defaultEngines;
+
+        }
+      }
     },
 
 
@@ -74,16 +75,24 @@ var exports = (function () {
           template: this.selectedTemplates
         };
 
-        _.forEach(this.parameters, function(value, key) {
-          if (value.includes(";")) {
-            params[key] = value.split(";");
+        // serialize parameters corresponding to https://publicwiki.deltares.nl/display/Delft3DGT/Search
+        params.parameters = _.map(
+          // loop over all parameters in the template
+          this.parameters,
+          function(value, key) {
+            var result = "";
+            if (value.includes(";")) {
+              result = key + "," + _.join(",", value.split(";"));
+            } else {
+              result = "key" + "," + value;
+            }
           }
-
-        });
-
+        );
         return {
           url: "/api/v1/scenes/",
           data: params,
+          // no [] in params
+          traditional: true,
           dataType: "json"
         };
       },
