9b0e26a139b62ca0480f7795b32b5a944665279e
Fedor Baart <fedor.baart@deltares.nl>
2016-10-12 11:54:36 +0200

    remove container

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -4,161 +4,99 @@ var exports = (function() {
   var store = {
 
     state: {
-      modelContainers: [],
-      models: [],
-      modelsFetched: false,
-      scenarioContainers: [],
-      scenarios: [],
-      scenariosFetched: false,
-      selectedModelContainer: undefined,
+      models: {},
+      scenarios: {},
       updateInterval: 2000,
+      interval: null,
       user: {id: -1}
     },
 
     // ================================ SYNCHRONISATION
 
     startSync: function () {
-      this.update(this)
-      this.interval = setInterval(this.update.bind(this), this.state.updateInterval);
+      this.update();
+      this.state.interval = setInterval(this.update.bind(this), this.state.updateInterval);
     },
     stopSync: function () {
-      clearInterval(this.interval);
-      this.interval = null;
+      clearInterval(this.state.interval);
+      this.stete.interval = null;
     },
 
     update: function () {
-      this.fetchModels().then(function (json) {
-        this.state.models = json;
-        this.updateContainers();
-      }.bind(this)).catch(function(reason) {
-        console.error('Promise rejected: ' + reason);
-      });
-      this.fetchScenarios().then(function (json) {
-        this.state.scenarios = json;
-        this.updateContainers();
-      }.bind(this)).catch(function(reason) {
-        console.error('Promise rejected: ' + reason);
-      });
+      this.fetchModels()
+        .then((json) => {
+          this.state.models = json;
+        })
+        .catch(function(reason) {
+          console.error("Promise rejected: " + reason);
+        });
+      this.fetchScenarios()
+        .then((json) => {
+          this.state.scenarios = json;
+        })
+        .catch(function(reason) {
+          console.error("Promise rejected: " + reason);
+        });
     },
 
     // ================================ API FETCH CALLS
 
     fetchModels: function () {
-      this.state.modelsFetched = false;
-      return new Promise(function(resolve, reject) {
+      return new Promise((resolve, reject) => {
         $.ajax({url: "/api/v1/scenes/", data: [], traditional: true, dataType: "json"})
-          .done(function(json) {
-            this.state.modelsFetched = true;
+          .done((json) => {
             resolve(json);
-          }.bind(this))
+          })
           .fail(function(error) {
             reject(error);
           });
-      }.bind(this));
+      });
     },
     fetchScenarios: function () {
-      this.state.scenariosFetched = false;
-      return new Promise(function(resolve, reject) {
+      return new Promise((resolve, reject) => {
         $.ajax({url: "/api/v1/scenarios/", data: [], traditional: true, dataType: "json"})
-          .done(function(json) {
-            this.state.scenariosFetched = true;
+          .done((json) => {
             resolve(json);
-          }.bind(this))
+          })
           .fail(function(error) {
             reject(error);
           });
-      }.bind(this));
-    },
-
-    // ================================ CONTAINER UPDATES
-
-    updateContainers: function () {
-      if(!this.state.scenariosFetched || !this.state.modelsFetched) {
-        return;
-      }
-      this.updateModelContainers();
-      this.updateScenarioContainers();
-    },
-    updateModelContainers: function () {
-      _.each(this.state.models, function (model) {
-        var container = _.find(this.state.modelContainers, ['id', model.id]);
-        if(container === undefined) {
-          // create new container
-          container = {'id': model.id, active: false, selected: false, data:model};
-          this.state.modelContainers.push(container);
-        } else {
-          // update model in container
-          container.data = model;
-        }
-      }.bind(this));
-
-      // TODO: remove containers for which there are no models
-    },
-    updateScenarioContainers: function () {
-      _.each(this.state.scenarios, function (scenario, index) {
-        var scenarioContainer = _.find(this.state.scenarioContainers, ['id', scenario.id]);
-        var modelContainerSet = _.filter(this.state.modelContainers, function (o) {
-          return _.includes(scenario.scene_set, o.id)
-        });
-        if(scenarioContainer === undefined) {
-          // create new scenarioContainer
-          scenarioContainer = {'id': scenario.id, data:scenario, models: modelContainerSet};
-          this.state.scenarioContainers.push(scenarioContainer);
-        } else {
-          // update scenario in scenarioContainer
-          scenarioContainer.data = scenario;
-          scenarioContainer.models = modelContainerSet;
-        }
-      }.bind(this));
-
-      _.each(this.state.scenarioContainers, function (container) {
-        if (container.models.length == 0) {
-          this.deleteScenario(container);
-        }
-      }.bind(this));
+      });
     },
 
     // ================================ API MODELS UPDATE CALLS
 
-    deleteModel: function (modelContainer) {
-      // snappyness: remove modelContainer from store
-      if(this.state.selectedModelContainer === modelContainer) {
-        this.state.selectedModelContainer = undefined;
-      }
-      this.state.modelContainers = _.without(this.state.modelContainers, modelContainer)
-      _.each(this.state.scenariosContainers, function (container) {
-        container.models = _.without(container.models, modelContainer);
-      });
-
+    deleteModel: function (model) {
+      // snappyness: remove model from store
       // update backend
-      $.ajax({url: "/api/v1/scenes/" + modelContainer.id + "/", method: "DELETE", data: [], traditional: true, dataType: "json"})
+      $.ajax({url: "/api/v1/scenes/" + model.id + "/", method: "DELETE", data: [], traditional: true, dataType: "json"})
         .done(function() {})
         .fail(function(error) {
           console.error(error);
         });
     },
 
-    publishModel: function (modelContainer, target) {
-      modelContainer.data.shared = 'u';
-      $.ajax({url: "/api/v1/scenes/" + modelContainer.id + "/publish_" + target + "/", method: "POST"})
+    publishModel: function (model, target) {
+      model.data.shared = "u";
+      $.ajax({url: "/api/v1/scenes/" + model.id + "/publish_" + target + "/", method: "POST"})
         .done(function () {})
         .fail(function(error) {
           console.error(error);
         });
     },
 
-    startModel: function (modelContainer) {
-      modelContainer.data.state = 'Queued';
-      $.ajax({url: "/api/v1/scenes/" + modelContainer.id + "/start/", method: "PUT", data: [], traditional: true, dataType: "json"})
+    startModel: function (model) {
+      model.data.state = "Queued";
+      $.ajax({url: "/api/v1/scenes/" + model.id + "/start/", method: "PUT", data: [], traditional: true, dataType: "json"})
         .done(function() {})
         .fail(function(error) {
           console.error(error);
         });
     },
 
-    stopModel: function (modelContainer) {
-      modelContainer.data.state = 'Stopping simulation...';
-      $.ajax({url: "/api/v1/scenes/" + modelContainer.id + "/stop/", method: "PUT", data: [], traditional: true, dataType: "json"})
+    stopModel: function (model) {
+      model.data.state = "Stopping simulation...";
+      $.ajax({url: "/api/v1/scenes/" + model.id + "/stop/", method: "PUT", data: [], traditional: true, dataType: "json"})
         .done(function() {})
         .fail(function(error) {
           console.error(error);
@@ -167,10 +105,9 @@ var exports = (function() {
 
     // ================================ API SCENARIOS UPDATE CALLS
 
-    deleteScenario: function (scenarioContainer) {
-      // snappyness: remove scenarioContainer from store
-      this.state.scenarioContainers = _.without(this.state.scenarioContainers, scenarioContainer)
-      $.ajax({url: "/api/v1/scenarios/" + scenarioContainer.id + "/", method: "DELETE", data: [], traditional: true, dataType: "json"})
+    deleteScenario: function (scenario) {
+      // snappyness: remove scenario from store
+      $.ajax({url: "/api/v1/scenarios/" + scenario.id + "/", method: "DELETE", data: [], traditional: true, dataType: "json"})
         .done(function() {})
         .fail(function(error) {
           console.error(error);
@@ -179,8 +116,9 @@ var exports = (function() {
 
     // ================================ OTHER SUPPORT METHODS
 
-    fetchLog: function (modelContainer) {
+    fetchLog: function (model) {
       // TODO: write fetchlog
+      console.warn("fetchlog not implemented, while trying to update model:", model);
     }
 
   };
