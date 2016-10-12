6f797024b96cac2d86d7bbd8498f76f8fc7ec191
Fedor Baart <fedor.baart@deltares.nl>
2016-10-12 20:01:05 +0200

    remove password

--- app/api/update.sh
+++ app/api/update.sh
@@ -1,5 +1,4 @@
 curl -c cookies.txt -b cookies.txt 'http://al-ng022.xtr.deltares.nl/login/'
 csrftoken=$(tail -n 1 cookies.txt  | cut  -f 7)
-curl -c cookies.txt -b cookies.txt 'http://al-ng022.xtr.deltares.nl/login/' --data "username=foo&password=foofoofoo&csrfmiddlewaretoken=$csrftoken"
-
+curl -c cookies.txt -b cookies.txt 'http://al-ng022.xtr.deltares.nl/login/' --data "username=foo&password=$PASSWORD&csrfmiddlewaretoken=$csrftoken"
 curl -c cookies.txt "http://al-ng022.xtr.deltares.nl/api/v1/scenarios/?format=json&csrfmiddlewaretoken=$csrftoken"
