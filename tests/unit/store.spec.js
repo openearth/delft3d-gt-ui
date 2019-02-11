fda745f7691290b4789f834d703d5a3cff9275cf
CindyvdVries <cindyvdvries@gmail.com>
2019-02-11 13:37:24 +0100

    update store test

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -1,37 +1,44 @@
 
+import chaiAsPromised from 'chai-as-promised'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
 import { expect } from 'chai'
-let store = require("../../src/store.js").store;
+import chai from 'chai'
+let store = require('../../src/store.js').store
+
+// setup chai
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+let should = chai.should()
 
 // test component
-describe("Store", function() {
-
-
+describe('Store', function () {
 // ***************************************************************************** update()
 
-describe(".update()", function() {
-  it("updates properly", function() {
-    expect(1).to.equal(1)
-  //   // stub and spy
-  //   sinon.stub(store, "fetchModels").returns(Promise.resolve());
-  //   sinon.stub(store, "fetchScenarios").returns(Promise.resolve());
-  //
-  //   // set store state to 'updating' and update
-  //   store.state.updating = true;
-  //   store.update();
-  //
-  //   // make sure no promises have been called
-  //   Promise.all.should.have.not.been.called;
-  //   store.fetchModels.should.have.not.been.called;
-  //   store.fetchScenarios.should.have.not.been.called;
-  //
-  //   // now set 'updating' state to false and call again
-  //   store.state.updating = false;
-  //   store.update();
-  //
-  //   // check that all promises have been called
-  //   Promise.all.should.have.been.called;
-  //   store.fetchModels.should.have.been.called;
-  //   store.fetchScenarios.should.have.been.called;
-  });
-});
+  describe('.update()', function () {
+    it('updates properly', function () {
+      expect(1).to.equal(1)
+      // stub and spy
+      sinon.stub(store, 'fetchModels').returns(Promise.resolve())
+      sinon.stub(store, 'fetchScenarios').returns(Promise.resolve())
+
+      // set store state to 'updating' and update
+      store.state.updating = true
+      store.dispatch('update')
+
+      // make sure no promises have been called
+      Promise.all.should.have.not.been.called
+      store.dispatch('fetchModels').should.have.not.been.called
+      store.dispatch('fetchScenarios').should.have.not.been.called
+
+      // now set 'updating' state to false and call again
+      store.state.updating = false
+      store.dispatch('update')
+
+      // check that all promises have been called
+      Promise.all.should.have.been.called
+      store.dispatch('fetchModels').should.have.been.called
+      store.dispatch('fetchScenarios').should.have.been.called
+    })
+  })
 })
