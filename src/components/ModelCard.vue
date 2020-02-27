8be7eb7a01d5370eb65ddfeb184580976c95e39e
CindyvdVries <cindyvdvries@gmail.com>
2020-02-27 15:04:34 +0100

    update model card for bootstrap layout

--- src/components/ModelCard.vue
+++ src/components/ModelCard.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="template-model-card">
-  <div class="model-card" :class="{'activated': model.active}" @click.stop="toggleActive">
-    <template v-if="model.data">
-      <div class="panel-body" :class="{ 'panel-active': model.active }">
-        <div class="row">
-          <div class="col-xs-8 col-sm-5">
-            <span v-if="model.data.shared == 'p'"><i class="fa fa-fw fa-user" aria-hidden="true"></i></span>
-            <span v-if="model.data.shared == 'c'"><i class="fa fa-fw fa-group" aria-hidden="true"></i></span>
-            <span v-if="model.data.shared == 'w'"><i class="fa fa-fw fa-globe" aria-hidden="true"></i></span>
-            &nbsp;{{model.data.name}}
-          </div>
-          <div class="col-xs-4 col-sm-4 text-right">
-            <span class="label label-status"
-                  :class="'label-' + model.statusLevel">
-              {{model.data.state.toUpperCase() }}
-            </span>
-          </div>
-          <div class="col-xs-11 col-sm-2 text-center no-padding">
-            <div class="progress">
-              <div
-                :data-percentage="`${model.data.progress}%`"
-                :style="{ width: `${model.data.progress}%`}"
-                class="progress-bar"
-                :class="[
+<div class="model-card card mb-2" :class="{'activated': model.active}" @click.stop="toggleActive">
+  <div class="card-body" :class="{ 'card-active': model.active }" v-if="model.data">
+    <div class="row">
+      <div class="col-xs-8 col-md-6">
+        <span v-if="model.data.shared == 'p'"><i class="fa fa-fw fa-user" aria-hidden="true"></i></span>
+        <span v-if="model.data.shared == 'c'"><i class="fa fa-fw fa-group" aria-hidden="true"></i></span>
+        <span v-if="model.data.shared == 'w'"><i class="fa fa-fw fa-globe" aria-hidden="true"></i></span>
+        &nbsp;{{model.data.name}}
+      </div>
+      <div class="col-xs-4 col-md-3 my-auto">
+        <span class="badge" :class="[
+                                     (model.statusLevel == 'Finished') ? 'badge-success' : '',
+                                     (model.statusLevel == 'Idle: waiting for user input') ? 'badge-warning' : '',
+                                     (model.statusLevel == 'Running simulation') ? 'badge-striped active' : '',
+                                     (
+                                     model.statusLevel != 'Finished' &&
+                                     model.statusLevel != 'Idle: waiting for user input'
+                                     ) ? 'badge-info' : '',
+                                     ]">
+          {{model.data.state.toUpperCase() }}
+        </span>
+      </div>
+      <div class="col-xs-11 col-md-2 m-auto p-0">
+        <div class="progress">
+          <div :data-percentage="`${model.data.progress}%`" :style="{ width: `${model.data.progress}%`}" class="progress-bar" :class="[
                          (model.data.state == 'Finished') ? 'bg-success' : '',
                          (model.data.state == 'Idle: waiting for user input') ? 'bg-warning' : '',
                          (model.data.state == 'Running simulation') ? 'bg-striped active' : '',
@@ -31,32 +32,20 @@
                          model.data.state != 'Idle: waiting for user input'
                          ) ? 'bg-info' : '',
                          ]"
-                role="progressbar"
-                aria-valuemin="0"
-                aria-valuemax="100"></div>
-            </div>
-          </div>
-          <div class="col-xs-1 col-sm-1 text-center">
-            <form class="form-inline">
-              <input type="checkbox" v-model="model.selected">
-            </form>
-          </div>
-
+            role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
-
       </div>
-
-    </template>
-
-    <template v-else>
-
-      model missing
-
-    </template>
-
+      <div class="col-xs-1 col-md-1 my-auto">
+        <form class="form-inline m-auto">
+          <input type="checkbox" v-model="model.selected">
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="card-body" v-else>
+    model missing
   </div>
 </div>
-
 </template>
 
 <script>
@@ -74,13 +63,13 @@ export default {
   },
 
   methods: {
-    toggleActive () {
+    toggleActive() {
       this.model.active = true
       bus.$emit('activated', this.model)
     }
   },
 
-  mounted () {
+  mounted() {
     bus.$on('deactivate', (clickedmodel) => {
       if (this.model !== clickedmodel) {
         this.model.active = false
@@ -101,32 +90,15 @@ export default {
 @import '../assets/variables.scss';
 
 .model-card {
-  cursor: pointer;
-  user-select: none;
+    cursor: pointer;
+    user-select: none;
 
-  .progress {
-    margin-bottom: 0;
-  }
-
-  .panel-active {
-    background-color: $col-bw-2;
-  }
-
-  .label {
-    float: right;
-  }
-
-  .row {
-    margin-bottom: 0;
-    // padding-bottom: 15px;
-  }
-
-  div {
-    [class^='col-'] {
-      margin-bottom: 0;
-      padding-bottom: 0;
+    .card-active {
+        background-color: $col-bw-2;
     }
-  }
-}
 
+    .label {
+        float: right;
+    }
+}
 </style>
